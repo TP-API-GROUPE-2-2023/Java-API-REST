1c863c12272b7420621c1c1a492a1b51886235df
Michel14XW <kpekpassimichel@gmail.com>
2023-05-14 12:31:17 +0000

    Premier commit du taf

---
+++ src/main/java/groupe2/tpApiRest/TpApiRestApplication.java
@@ -0,0 +1,13 @@
+package groupe2.tpApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpApiRestApplication.class, args);
+	}
+
+}
